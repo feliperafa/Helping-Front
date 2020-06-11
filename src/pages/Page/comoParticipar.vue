c20e3fbac356f021d0ed94ae97476e8e6bc96c54
feliperafa <feliperafael_barros@hotmail.com>
2020-06-11 20:41:13 -0300

    Criacao da pagina como participar

--- src/pages/Page/comoParticipar.vue
+++ src/pages/Page/comoParticipar.vue
@@ -1,3 +1,35 @@
 <template>
-  <H1>Como Participar Teste</H1>
+<q-page>
+  <div class="q-ma-md logo text-center">
+        <h4>Como Participar</h4>
+      <div class="L">
+Desde o início de março, a única certeza que temos é a incerteza dos dias.
+
+A pandemia do COVID-19 e a necessidade do isolamento social deixaram todos vulneráveis – seja no quesito saúde mental financeiro ou então de segurança. Por isso, vários projetos nasceram da necessidade de ajudar quem mais precisa. Aqui em Santos, a iniciativa mais recente é a Helping SomeBody.<br>
+Com isso Criamos um sistema capaz de ajudar quem precisa, de formas diretas e indiretas<br>
+Para Participar de Forma <strong>Indireta</strong>, e ajudar quem precisa fazendo uma doação para uma de nossas ONGs já, cadastradas em nosso sistema é muito Simples basta seguir o Passo a passo.<br>
+<strong>1°</strong> Basta entra no sistema, se dirigir ate a aba de projetos, escolher a ong no qual deseja ajudar, e fazer a doação de forma Automatica os valores variam entre <strong>R$ 10,00 , 20,00 , 30,00 40,00 ou até 50,00.</strong><br>
+Fazendo uma doação voce vai esta ajudando quem precisa e ainda ajudando a manter a iniciativa da plataforma Online, os valores arrecadados tem um desconto de <strong>10%</strong> em cada doação que é voltada para o sistema para ajuar na manutenção e os <strong>90%</strong> da doação é voltado para a ong da sua escolha.
+
+      </div>
+        <h4>Como Ser um Voluntario ou Doador Direto</h4>
+      <div class="L">
+Para ser um <a class="text-body-2 q-mt-lg" href="/projetos"><em>Doador</em></a> direto ou um voluntario é muito Simples, basta verificar qual a ONG que chamou mais a sua atenção ou a que voce mais se identificou com a causa, em seguida Basta Clicar em quero ser voluntario onde voce ira ser movido para outra pagina onde ira fazer seu cadastro e a qualquer momento uma de nossas onges cadastradas pode entra em contato solicitando os serviços, de acordo com a sua expertize ou função de dominio para ajudar, sendo com prestação de serviço ou ate mesmo com ajuda social caso voce seja um profissional de qualquer area.
+      </div>
+        <h4>Parceiros ou Empresa Parceira</h4>
+      <div class="L">
+        A covid-19 está mudando a vida de todo mundo. Mas, para algumas pessoas, o impacto está sendo maior, colocando-as em uma condição mais vulnerável. E, nesses tempos de isolamento social, podemos aprender a importância da união.
+
+Pensando nisso, a Helping SomeBody te convida e convida sua empresa para o projeto <strong>#ColaNessaCausa</strong>, uma iniciativa para ajudar pessoas que estão passando ainda mais dificuldades por conta do novo coronavírus, com doações de pratos de comida* que serão distribuídos entre as instituiçoes parceiras.
+
+Como pontapé inicial dessa iniciativa, as Empresas que desejam participar do projeto podem doar um valor de R$ 60,00 reais onde Será convertido da seguinte forma <strong> R$ 10,00</strong> reais que será o equivalente a uma quentinha que iremos doar para as instituiçoes parceiras e <strong>R$ 50,00</strong> reais mensal será para manter sua propaganda ou a propaganda da sua empresa em nosso sistema.
+
+A Helping SomeBody impulsionou a campanha com a ideia de que seria só o começo e que essa ajuda poderia ser muito maior. Então, aproveitamos a nossa paixão por ajudar e pensamos em fazer isso juntos.
+
+Basta Entra em contato com nosco atraves, do E-mail <Strong>Helpingsomebody@gmail.com</Strong> e no titulo do email deve colocar a seguinte frase APOIAR + o nome da sua empresa juntamente com o seus dados para contato. <br>
+<strong>#ColaNessaCausa</strong> você também!
+
+      </div>
+  </div>
+  </q-page>
 </template>
